d423c080d803f1c3ead3bfe5a07450cc32cd20bf
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-14 19:11:16 -0300

    Atualização para JavaScript
    Anteriormente com o comando:
    npx create-expo-app florestas-pelo-mundo --template expo-template-blank@sdk-50 estava gerando erro e não gerava a Aplicação
    
    Depois com o comando: npx create-expo-app florestas-pelo-mundo e um segundo comando: npm run reset-project. Criava um aplicativo TypeScript
    
    Resolvi o problema retirando do comando original <@sdk-50> ficando o commando que gera o template em branco: npx create-expo-app florestas-pelo-mundo  --template expo-template-blank
    
    Por último segui conforme orientação:
    npm install expo@latest
    npx expo install --fix
    npx expo start

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import Congo from './assets/images/floresta-congo.png';
+import Taiga from './assets/images/floresta-taiga.png';
+import Amazonica from './assets/images/floresta-amazonica.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ estilos.container }>
+      <Text style={ estilos.titulo }>Florestas pelo mundo</Text>
+
+      <Image source={ Congo } style={ estilos.img } />
+      <Text style={[ estilos.margem, estilos.textoCongo ]}>Floresta do congo (África)</Text>
+
+      <Image source={ Taiga } style={ estilos.img } />
+      <Text style={[ estilos.margem, estilos.textoNorte ]}>Floresta (Hemisfério Norte)</Text>
+
+      <Image source={ Amazonica } style={ estilos.img } />
+      <Text style={[ estilos.margem, estilos.textoSul ]}>Floresta Amazônica (América do Sul)</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo: {
+    fontSize: 22,
+    marginBottom: 30,
+    width: 280,
+  },
+
+  img: {
+    width: 300,
+    height: 100,
+  },
+
+  margem: {
+    marginBottom: 20,
+  },
+
+  textoNorte: {
+    width: 240,
+  },
+
+  textoCongo: {
+    width: 230,
+  },
+
+  textoSul: {
+    fontSize: 11,
+    width: 260,
+  }
 });
